633dd83cdf5f3f83db14a90221c7b9f22eed3414
Hging <hging3@gmail.com>
2021-09-10 14:45:39 +0800

    [WIP] upgrade to 5f73ba1f67c33d7e92a66e78a3cfd1132fa5a7ad

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -578,9 +578,6 @@ impl_runtime_apis! {
 			data.check_extrinsics(&block)
 		}
 
-		fn random_seed() -> <Block as BlockT>::Hash {
-			RandomnessCollectiveFlip::random_seed().0
-		}
 	}
 
 	impl sp_transaction_pool::runtime_api::TaggedTransactionQueue<Block> for Runtime {
